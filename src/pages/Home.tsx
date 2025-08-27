2375a52f8c4f2d0fddf30b35d9092af25edfc27a
hamadalhibshi <hamad.alhibshi@gmail.com>
2025-08-27 14:27:58 +0300

    Navigation working

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Typography } from "@mui/material";
+
+const Home = () => {
+    return (
+        <div>
+            <Typography color="orange">Home</Typography>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Typography } from "@mui/material";
+
+const NotFound = () => {
+    return (
+        <div>
+            <Typography color="orange">sad</Typography>
+        </div>
+    )
+}
+
+export default NotFound
