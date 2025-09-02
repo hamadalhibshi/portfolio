ede5eb3e20898c17b139db0d47bbf7aff580ef06
hamadalhibshi <hamad.alhibshi@gmail.com>
2025-09-02 13:09:11 +0300

    Added icon pack

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import type { ButtonProps } from "../types"
 const Button = ({href, className, children, onClick}: ButtonProps) => {
     const classes = `${className 
         ? '' 
-        : 'px-8 py-5 rounded-2xl bg-blue-600 text-white hover:bg-blue-700'}`
+        : 'px-8 py-5 rounded-lg bg-blue-600 text-white hover:bg-blue-700 cursor-pointer'}`
     const spanClasses = `relative z-10 uppercase`
 
     const renderButton = () => {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,45 @@
 import { Button } from "../components";
 import { homeText } from "../constants";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { LuGithub } from "react-icons/lu";
 
 const Home = () => {
+    const iconClass = 'flex bg-blue-500 rounded-lg w-12 h-12 items-center justify-center cursor-pointer hover:bg-blue-700';
+
+    const openGithubUrl = () => {
+        window.open('https://github.com/hamadalhibshi', '_blank')
+    }
+
+    const openLinkedinUrl = () => {
+        window.open('https://www.linkedin.com/in/halhibshi/', '_blank')
+    }
+
+
     return (
         <div className="py-30 m-10 flex">
             <div className="w-2/3">
                 <p className="">{homeText.introduction}</p>
                 <h1 className="uppercase text-5xl mt-5 font-bold">{homeText.name}</h1>
-                <h2 className="uppercase text-5xl mt-5 text-gray-400">{homeText.role}</h2>
+                <h1 className="uppercase text-5xl mt-5 text-gray-400">{homeText.role}</h1>
                 <p className="w-3/4 mt-10 leading-relaxed text-justify text-2xl">{homeText.about}</p>
 
                 <div className="flex w-2/5 justify-between mt-10">
                     <Button onClick={() => {}}>{homeText.donwloadResume}</Button>
                     <Button onClick={()=> {}}>{homeText.contact}</Button>
+                </div>
+
+                <div className="flex mt-10 items-center justify-between w-30">
+                    <a onClick={openGithubUrl}>
+                        <div className={iconClass}>
+                            <LuGithub size={30} />
+                        </div>
+                    </a>
+
+                    <a onClick={openLinkedinUrl}>
+                        <div className={iconClass}>
+                            <TiSocialLinkedin size={35} />
+                        </div>
+                    </a>
                 </div>
             </div>
 
