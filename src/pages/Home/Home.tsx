f2b80ddc2d1f4cf52b8b8d3f8f5f26d8ab4c0bfa
hamadalhibshi <hamad.alhibshi@gmail.com>
2025-09-08 01:54:24 +0300

    Fixed prettier issue

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,91 +12,98 @@ const Home = () => {
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
-    const tl = gsap.timeline({ repeat: -1});
+    const tl = gsap.timeline({ repeat: -1 });
     gsap.fromTo(
-        '#intro-text',
-        { opacity: 0, x: -20},
-        {
-            opacity: 1,
-            x: 0,
-            delay: 0.2,
-            duration: 1.5,
-            ease: "power3.out",
-        }
+      "#intro-text",
+      { opacity: 0, x: -20 },
+      {
+        opacity: 1,
+        x: 0,
+        delay: 0.2,
+        duration: 1.5,
+        ease: "power3.out",
+      }
     );
     gsap.fromTo(
-        '#name-text',
-        { opacity: 0, x: -20},
-        {
-            opacity: 1,
-            x: 0,
-            delay: 0.5,
-            duration: 1.5,
-            ease: "power3.out",
-        }
+      "#name-text",
+      { opacity: 0, x: -20 },
+      {
+        opacity: 1,
+        x: 0,
+        delay: 0.5,
+        duration: 1.5,
+        ease: "power3.out",
+      }
     );
     gsap.fromTo(
-        "#actual-role-text",
-        { opacity: 0, x: -20 },
-        {
-            opacity: 1,
-            x: 0,
-            duration: 1.5,
-            delay: 1,
-            ease: "power3.out",
-            scrambleText: {
-            text: scrambleTextRoles[0],
-            chars: "upperCase",
-            speed: 0.5
-            }
-        }
+      "#actual-role-text",
+      { opacity: 0, x: -20 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1.5,
+        delay: 1,
+        ease: "power3.out",
+        scrambleText: {
+          text: scrambleTextRoles[0],
+          chars: "upperCase",
+          speed: 0.5,
+        },
+      }
     );
     scrambleTextRoles.forEach((role) => {
-      tl.to("#actual-role-text", {
-        scrambleText: {
-          text: role,
-          chars: "upperCase",
-          speed: 0.5
+      tl.to(
+        "#actual-role-text",
+        {
+          scrambleText: {
+            text: role,
+            chars: "upperCase",
+            speed: 0.5,
+          },
+          duration: 2,
         },
-        duration: 2
-      }, "+=1");
+        "+=1"
+      );
     });
     gsap.fromTo(
-        '#about-text',
-        { opacity: 0, x: -20},
-        {
-            opacity: 1,
-            x: 0,
-            delay: 1.5,
-            duration: 1.5,
-            ease: "power3.out",
-        }
+      "#about-text",
+      { opacity: 0, x: -20 },
+      {
+        opacity: 1,
+        x: 0,
+        delay: 1.5,
+        duration: 1.5,
+        ease: "power3.out",
+      }
     );
     gsap.fromTo(
-        "#wheel-icon",
-        { opacity: 0, rotation: 0, y: 500 },
-        {
-          opacity: 1,
-          y: 0,
-          rotation: 1440,
-          ease: "none",
-          scrollTrigger: {
-            trigger: "#wheel-icon",
-            start: "top 80%",
-            end: "+=1200",
-            scrub: true,
-            // markers: true
-          }
-        }
-      );
+      "#wheel-icon",
+      { opacity: 0, rotation: 0, y: 500 },
+      {
+        opacity: 1,
+        y: 0,
+        rotation: 1440,
+        ease: "none",
+        scrollTrigger: {
+          trigger: "#wheel-icon",
+          start: "top 80%",
+          end: "+=1200",
+          scrub: true,
+          // markers: true
+        },
+      }
+    );
   }, []);
 
   return (
     <div>
       <div className="py-30 m-10 flex pb-24">
         <div className="w-2/3">
-          <p id='intro-text'>{homeText.introduction}</p>
-          <h1 id='name-text' className="uppercase text-5xl mt-5 font-bold text-neutral-50">
+          <p id="intro-text">{homeText.introduction}</p>
+          <h1
+            id="name-text"
+            className="uppercase text-5xl mt-5 font-bold text-neutral-50"
+          >
             {homeText.name}
           </h1>
           <h1
@@ -105,14 +112,17 @@ const Home = () => {
           >
             {homeText.role}
           </h1>
-          <p id='about-text' className="w-3/4 mt-10 leading-relaxed text-justify text-2xl">
+          <p
+            id="about-text"
+            className="w-3/4 mt-10 leading-relaxed text-justify text-2xl"
+          >
             {homeText.about}
           </p>
         </div>
 
         <div className="justify-center items-center">
           <img src="" className="w-130 h-130 rounded-full bg-amber-300" />
-          <GiCarWheel id='wheel-icon' className="w-130 h-130" color="white" />
+          <GiCarWheel id="wheel-icon" className="w-130 h-130" color="white" />
         </div>
       </div>
 
--- src/pages/Home/components/Projects.tsx
+++ src/pages/Home/components/Projects.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { projects } from "../../../constants"
+import { projects } from "../../../constants";
 import { gsap } from "gsap";
 
 const Projects = () => {
@@ -84,74 +84,105 @@ const Projects = () => {
         }
       );
     });
-      gsap.fromTo(
-        "#title",
-        { opacity: 0, y: -80, scale: 0.8, rotationX: 45, skewY: 5 },
-        {
-          opacity: 1,
-          y: 0,
-          scale: 1,
-          rotationX: 0,
-          skewY: 0,
-          ease: "back.out(1.7)",
-          scrollTrigger: {
-            trigger: "#title",
-            start: "top 80%",
-            end: "top 30%",
-            scrub: true,
-            // markers: true,
-          }
-        }
-      );
+    gsap.fromTo(
+      "#title",
+      { opacity: 0, y: -80, scale: 0.8, rotationX: 45, skewY: 5 },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        rotationX: 0,
+        skewY: 0,
+        ease: "back.out(1.7)",
+        scrollTrigger: {
+          trigger: "#title",
+          start: "top 80%",
+          end: "top 30%",
+          scrub: true,
+          // markers: true,
+        },
+      }
+    );
   }, []);
 
   const renderView = (projects: any) => {
     return (
       <>
-          {projects.map((project: any, index: number) => {
-            const isOdd = project.id % 2 !== 0;
+        {projects.map((project: any, index: number) => {
+          const isOdd = project.id % 2 !== 0;
 
-            return (
-                <div key={index} className="flex mb-8 flex-wrap justify-center items-center">
-                  {!isOdd && (
-                    <div id='image' className="w-full lg:w-130 lg:h-80">
-                        <img className="mb-2 w-full h-full rounded-sm" src={project.img} />
-                    </div>
-                  )}
-
-                  <div className={`lg:w-2/5 ${isOdd ? 'lg:-mr-20' : 'lg:-ml-20'}`}>
-                    <h6 id="project-title" className={`mb-5 font-semibold ${isOdd ? 'text-start' : 'text-end'} text-2xl`}>{project.title}</h6>
-                    <div id='description' className="w-full h-50 bg-gray-600 p-5 rounded-sm relative z-10 shadow-lg">
-                      <p className="mb-4">{project.description}</p>
-                    </div>
-
-                    <div id="project-tech" className={`w-full mt-5 flex ${isOdd ? 'justify-start' : 'justify-end'}`}>
-                      {project.tech.map((tech: any, index: number) => (
-                        <span key={index} className={`${isOdd ? `mr-2` : `ml-2`} rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800`}>
-                          {tech}
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                  
-                  {isOdd && (
-                    <div id='image' className="w-full lg:w-130 lg:h-80">
-                      <img className="mb-2 w-full h-full rounded-sm" src={project.img} />
-                    </div>
-                  )}
+          return (
+            <div
+              key={index}
+              className="flex mb-8 flex-wrap justify-center items-center"
+            >
+              {!isOdd && (
+                <div id="image" className="w-full lg:w-130 lg:h-80">
+                  <img
+                    className="mb-2 w-full h-full rounded-sm"
+                    src={project.img}
+                  />
                 </div>
-              )
-          })}
+              )}
+
+              <div className={`lg:w-2/5 ${isOdd ? "lg:-mr-20" : "lg:-ml-20"}`}>
+                <h6
+                  id="project-title"
+                  className={`mb-5 font-semibold ${
+                    isOdd ? "text-start" : "text-end"
+                  } text-2xl`}
+                >
+                  {project.title}
+                </h6>
+                <div
+                  id="description"
+                  className="w-full h-50 bg-gray-600 p-5 rounded-sm relative z-10 shadow-lg"
+                >
+                  <p className="mb-4">{project.description}</p>
+                </div>
+
+                <div
+                  id="project-tech"
+                  className={`w-full mt-5 flex ${
+                    isOdd ? "justify-start" : "justify-end"
+                  }`}
+                >
+                  {project.tech.map((tech: any, index: number) => (
+                    <span
+                      key={index}
+                      className={`${
+                        isOdd ? `mr-2` : `ml-2`
+                      } rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800`}
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              {isOdd && (
+                <div id="image" className="w-full lg:w-130 lg:h-80">
+                  <img
+                    className="mb-2 w-full h-full rounded-sm"
+                    src={project.img}
+                  />
+                </div>
+              )}
+            </div>
+          );
+        })}
       </>
-    )
-  }
+    );
+  };
 
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h1 id='title' className="my-20 text-4xl text-center">Projects</h1>
+      <h1 id="title" className="my-20 text-4xl text-center">
+        Projects
+      </h1>
       {renderView(projects)}
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
