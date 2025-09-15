14aab558962a8855fc012c165f927741307082f1
hamadalhibshi <hamad.alhibshi@gmail.com>
2025-09-15 14:06:10 +0300

    Fixed nav bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,7 +3,7 @@ import { LuGithub } from "react-icons/lu";
 
 const NavigationBar = () => {
   const iconClass =
-    "flex rounded-lg w-12 h-12 items-center justify-center cursor-none hover:opacity-50";
+    "flex rounded-lg w-10 h-10 md:w-12 md:h-12 items-center justify-center cursor-none hover:opacity-50";
 
   const openGithubUrl = () => {
     window.open("https://github.com/hamadalhibshi", "_blank");
@@ -15,21 +15,21 @@ const NavigationBar = () => {
 
   return (
     <div className="top-0 w-full p-5 pt-10">
-      <div className="flex items-center justify-between ml-5 mr-5">
-        <div className="text-2xl uppercase">
+      <div className="flex items-center justify-between mx-5">
+        <div className="text-[15px] lg:text-2xl uppercase">
           <a href="/">Hamad Alhibshi</a>
         </div>
 
         <div>
           <button onClick={openGithubUrl}>
             <div className={iconClass}>
-              <LuGithub size={30} />
+              <LuGithub className="w-[20px] h-[20px] md:w-[30px] md:h-[30px]" />
             </div>
           </button>
 
           <button onClick={openLinkedinUrl}>
             <div className={iconClass}>
-              <TiSocialLinkedin size={30} />
+              <TiSocialLinkedin className="w-[20px] h-[20px] md:w-[30px] md:h-[30px]" />
             </div>
           </button>
         </div>
