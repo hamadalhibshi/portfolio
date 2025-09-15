a160253a857636ca630454e16df6b1373c6cc8db
hamadalhibshi <hamad.alhibshi@gmail.com>
2025-09-15 17:13:45 +0300

    fixed ipad view

--- src/pages/Home/components/MovableObject.tsx
+++ src/pages/Home/components/MovableObject.tsx
@@ -18,7 +18,7 @@ const MovableObject = () => {
       id="object"
       className="
       absolute w-[300px] h-[300px] left-1/2 -translate-x-1/2 top-[6%]
-      sm:w-[330px] sm:h-[330px] sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2
+      sm:w-[330px] sm:h-[330px] sm:top-[5%] sm:left-1/2 sm:-translate-x-1/2]
       md:w-[700px] md:h-[700px]
       lg:top-[15%] lg:right-0 lg:left-auto lg:translate-x-0 lg:-translate-y-0
       z-[-10]"
