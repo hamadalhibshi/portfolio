d4be0181fc3ff4820ca0496103f0fcda6355bcf1
hamadalhibshi <hamad.alhibshi@gmail.com>
2025-09-17 13:20:27 +0300

    removed unused code

--- src/pages/Home/components/index.ts
+++ src/pages/Home/components/index.ts
@@ -1,6 +1,5 @@
 export {default as Experience} from './Experience'
 export {default as Projects} from './Projects'
-export {default as MovableObject} from './MovableObject'
 export {default as TechCards} from './TechCards'
 export {default as About} from './About'
 export {default as Hero} from './Hero'
